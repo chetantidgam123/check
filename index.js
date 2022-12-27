85e481c0a6a8126b3cbdf6bd16a406122dad7dad
chetan <ctidgam1997@gmail.com>
2022-12-27 23:44:43 +0530

    index change

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const cors = require('cors');
 const productRouter = require('./router/product.router');
 const connect = require('./db/connect');
 const app = express()
-app.use(cors())
-
-
+app.use(function(req, res, next) {  
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }); 
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors({origin:true,credentials:true}));
 app.use('/',productRouter)
 
 
