63f70f0dee5ff7c2af7993f09723bd2f834eb8e7
chetan <ctidgam1997@gmail.com>
2023-01-03 02:00:06 +0530

    done

--- controller/cartitem.controller.js
+++ controller/cartitem.controller.js
@@ -7,7 +7,7 @@ const { CartItemmodel } = require("../model/cartitem.model");
 const token_secret = process.env.TOKEN_KEY;
 
 const addToCart = async (req, res) => {
-    let {id} = req.params;
+    let { id } = req.params;
     // let item_id = item._id.toString()
     let Bearer = req.headers["authorization"]
     let splittoken = Bearer.split(" ")
@@ -25,6 +25,7 @@ const addToCart = async (req, res) => {
                 let existing_prod = await CartItemmodel.findOne({ "cartItem.cartId": id });
                 if (existing_prod) {
                     return res.send({
+                        code: 404,
                         message: "Item Already in Cart"
                     })
                 } else {
@@ -46,13 +47,14 @@ const addToCart = async (req, res) => {
                                     color: product.color,
                                     size: product.size,
                                     stock: product.stock,
-                                    Qty:1
+                                    Qty: 1
                                 }
                             }
                         },
                         { new: true }
                     )
                     return res.send({
+                        code: 200,
                         message: "Item Added in cart",
                         data: cart
                     })
@@ -61,7 +63,7 @@ const addToCart = async (req, res) => {
             } else {
                 let cart = await CartItemmodel.create({
                     userId: a,
-                     cartItem: [
+                    cartItem: [
                         {
                             cartId: id,
                             name: product.name,
@@ -76,11 +78,12 @@ const addToCart = async (req, res) => {
                             color: product.color,
                             size: product.size,
                             stock: product.stock,
-                            Qty:1
+                            Qty: 1
                         }
                     ]
                 })
                 return res.send({
+                    code: 200,
                     message: "User Item Added in cart",
                     data: cart
                 })
@@ -89,8 +92,9 @@ const addToCart = async (req, res) => {
         }
     }
     catch (err) {
-        console.log(err);
         res.send({
+            code: 404,
+            message: "Invalid Token",
             err
         })
     }
@@ -105,76 +109,101 @@ const getUserCart = async (req, res) => {
         if (decode) {
             let userId = decode.userId.toString()
             let userCart = await CartItemmodel.findOne({ userId: userId });
-            let cartItem = userCart.cartItem || []
-           return res.send({cartItem})
+            let cartItem = userCart?.cartItem || []
+            return res.send({ cartItem })
         }
     }
     catch (err) {
         console.log(err);
-       return res.send({
+        return res.send({
             err
         })
     }
 }
 
-const updateCartQty = async(req,res)=>{
+const updateCartQty = async (req, res) => {
     let Bearer = req.headers["authorization"]
     let splittoken = Bearer.split(" ")
     let token = splittoken[1].replace('"', '');
-const {id} = req.params 
-const {Qty} = req.body
-try {
-    if(Qty>0){
-        let existing_prod = await CartItemmodel.findOneAndUpdate({ "cartItem.cartId": id },
-                                            {'$set': {
-                                            "cartItem.$.Qty":Qty
-                                         } ,
-                                        },
-                                { new: true }
-        );
+    const { id } = req.params
+    const { Qty } = req.body
+    try {
+        if (Qty > 0) {
+            let existing_prod = await CartItemmodel.findOneAndUpdate({ "cartItem.cartId": id },
+                {
+                    '$set': {
+                        "cartItem.$.Qty": Qty
+                    },
+                },
+                { new: true }
+            );
             var decode = jwt.verify(token, token_secret)
             if (decode) {
                 let userId = decode.userId.toString()
                 let userCart = await CartItemmodel.findOne({ userId: userId });
-                let cartItem = userCart.cartItem
-               return res.send({ cartItem, 
-                message:"Item added Succesfully"})
-            }
-    }else{
-        CartItemmodel.findByIdAndUpdate(
-            { "cartItem.cartId": id },
-           { $pull: { 'cartItem': {  cartId: id } } },function(err,model){
-              if(err){
-                   console.log(err);
-                   return res.send(err);
-                }
-                return res.json(model);
-            });
-        
-        // let existing_prod = await CartItemmodel.findOneAndRemove({ "cartItem.cartId": id },{new:true})
-            var decode = jwt.verify(token, token_secret)
-            if (decode) {
-                let userId = decode.userId.toString()
-                let userCart = await CartItemmodel.findOne({ userId: userId });
-                let cartItem = userCart.cartItem || []
-               return res.send({
-                cartItem, 
-                message:"Item Removed Succesfully"
-            })
-            }
-            else{
+                let cartItem = userCart?.cartItem || []
                 return res.send({
-                    message:"Token expired"
-                })  
+                    cartItem,
+                    message: ""
+                })
             }
-       
-    } 
-} catch (error) {
-    res.send(error)
-}
+        } else {
+
+            var decode = jwt.verify(token, token_secret)
+            if (decode) {
+                let userId = decode.userId.toString()
+                let deletedItem = await CartItemmodel.findOneAndUpdate(
+                    { userId: userId },
+                    { $pull: { cartItem: { cartId: id } } }
+                )
+                let userCart = await CartItemmodel.findOne({ userId: userId });
+                let cartItem = userCart?.cartItem || []
+                return res.send({
+                    cartItem,
+                    message: "Item Removed Succesfully"
+                })
+            }
+        }
+    } catch (error) {
+        res.send(error)
+    }
 
 }
 
+
+const clearCart = async (req, res) => {
+    let Bearer = req.headers["authorization"]
+    console.log(Bearer);
+    let splittoken = Bearer.split(" ")
+    let token = splittoken[1].replace('"', '');
+    try {
+        var decode = jwt.verify(token, token_secret)
+        if (decode) {
+            let userId = decode.userId.toString()
+           let data =  await CartItemmodel.findOneAndUpdate(
+                { userId: userId },
+                { $set: { cartItem: [] } },
+                {new:true}
+            )
+            if(data){
+                let userCart = await CartItemmodel.findOne({ userId: userId });
+                let cartItem = userCart?.cartItem || []
+                return res.send({
+                    cartItem,
+                    message: ""
+                })
+            }else{
+                return res.send({
+                    cartItem,
+                    message: ""
+                }) 
+            }
+        }
+
+    } catch (error) {
+        return res.send(error)
+    }
+}
 const getCart = async (req, res) => {
     let data = await CartItemmodel.find()
     return res.send({
@@ -185,5 +214,6 @@ module.exports = {
     addToCart,
     getCart,
     getUserCart,
-    updateCartQty
+    updateCartQty,
+    clearCart
 }
